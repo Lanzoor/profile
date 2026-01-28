61b38f3730fb8e21970e342c607dffdecf39c0ba
lanzoor <lanzoorgaming@gmail.com>
2026-01-28 10:18:33 +0900

    Implement scrolling animation

--- main.js
+++ main.js
@@ -74,3 +74,17 @@ function updateSecretDisplay() {
 }
 
 setInterval(updateSecretDisplay, 250);
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        }
+    });
+});
