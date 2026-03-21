b21d48ab99636182e635d0248c5584d96b72b953
lanzoor <lanzoorgaming@gmail.com>
2026-03-21 16:04:18 +0900

    Stage commits for v26.11.2: add stellar animation and name display

--- src/etc/welcome.ts
+++ src/etc/welcome.ts
@@ -1,7 +1,17 @@
 import { pingServer } from '../ping-server.js';
 
+let enableAnimation = true;
+
 async function sleep(timeMs: number): Promise<any> {
     return new Promise((p) => setTimeout(p, timeMs));
+}
+
+function pickRandom<T>(arr: T[]): T {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function randInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -70,4 +80,93 @@ document.addEventListener('DOMContentLoaded', async () => {
         statusDisplay.textContent = discordStatus;
         statusDisplay.classList.add(`status-${discordStatus}`);
     }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('profile--name-display')!;
+    const animationToggle = container.querySelector('#profile--name-display button')!;
+
+    animationToggle.addEventListener('click', () => {
+        enableAnimation = !enableAnimation;
+        animationToggle.textContent = enableAnimation ? '⏸ Pause animation' : '▶ Resume animation';
+    });
+
+    const stars: HTMLElement[] = [];
+
+    function randomizeStar(star: HTMLElement, initial = false) {
+        const size = Math.random() * 6;
+        const opacity = randInt(3, 10) / 10;
+
+        star.style.width = star.style.height = size + 'px';
+        star.style.left = Math.random() * 100 + '%';
+        star.dataset.top = initial ? String(Math.random() * 100) : String(100 + Math.random() * 20);
+
+        star.style.top = star.dataset.top + '%';
+        star.style.opacity = String(opacity);
+        star.dataset.speed = String(opacity * 0.3);
+    }
+
+    for (let i = 0; i < 100; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+
+        randomizeStar(star, true);
+
+        container.appendChild(star);
+        stars.push(star);
+    }
+
+    function update() {
+        if (enableAnimation) {
+            for (const star of stars) {
+                let top = parseFloat(star.dataset.top!);
+                const speed = parseFloat(star.dataset.speed!);
+
+                top -= speed;
+
+                if (top < -10) {
+                    star.style.transition = 'none';
+                    randomizeStar(star);
+
+                    star.getBoundingClientRect();
+
+                    star.style.transition = '';
+                    continue;
+                }
+
+                star.dataset.top = String(top);
+                star.style.top = top + '%';
+            }
+        }
+
+        requestAnimationFrame(update);
+    }
+
+    update();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const lanzoorLetters = Array.from(document.querySelectorAll('#profile--name-display span')).map((span) => span as HTMLSpanElement);
+    const fonts = ['Geist', 'Space Grotesk', 'JetBrains Mono', 'Fira Code', 'Fairfax HD', 'Brass Mono'];
+    let index = 0;
+
+    setInterval(() => {
+        if (!enableAnimation) {
+            return;
+        }
+
+        let currentFont = fonts[index];
+        let currentFontWeight = pickRandom(['100', '200', '300', '400']);
+        let currentFontStyle = pickRandom(['normal', 'italic']);
+        let currentFontSize = randInt(100, 150);
+
+        for (const letter of lanzoorLetters) {
+            letter.style.fontFamily = currentFont;
+            letter.style.fontWeight = currentFontWeight;
+            letter.style.fontStyle = currentFontStyle;
+            letter.style.fontSize = String(currentFontSize) + 'px';
+        }
+
+        index = (index + 1) % fonts.length;
+    }, 750);
 });
